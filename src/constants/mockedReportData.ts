b72d906a1710bed3722bc951694bcff390d8c532
Aibek_Saaliev <dokaidea@gmail.com>
2023-08-02 16:46:05 +0600

    #10 - Updated mocked data according to expanded DataType

--- src/constants/mockedReportData.ts
+++ src/constants/mockedReportData.ts
@@ -47,8 +47,9 @@ export const ExpensesData: MockedDataType = {
 export const EmployeesData: MockedDataType = {
     header: [
         {id: "project_name", type: "string", caption: "Project name", align: "center"},
+        {id: "run_status", type: "boolean", caption: "Run Status", align: "center"},
         {id: "total_by_project", type: "float", caption: "Total by project", align: "center"},
-        {id: "project_type", type: "string", caption: "Project type", align: "center"},
+        {id: "project_type", type: "string", caption: "Project type", align: "right"},
         {id: "jan", type: "float", caption: "Jan"},
         {id: "feb", type: "float", caption: "Feb"},
         {id: "mar", type: "float", caption: "Mar"},
@@ -56,17 +57,17 @@ export const EmployeesData: MockedDataType = {
         {id: "may", type: "float", caption: "May"},
         {id: "jun", type: "float", caption: "June"},
         {id: "jul", type: "float", caption: "July"},
-        {id: "aug", type: "float", caption: "August"},
+        {id: "aug", type: "float", caption: "Aug"},
     ],
     data: [
-        ["Online-School", 1024.3, "commercial", 2048.2, 4096.0, 1058.1, 941.7, 4519.4, 2013.1, 513.1, 907.3],
-        ["City-Scope", 2345.1, "internal", 3465.3, {d: 12.5, color: "selected"}, 547.0, 5416.1, 1654.8, 987.4, 500.0, 789.5],
-        ["Spotify", 54.4, "commercial", {d: 5489.1, color: "selected"}, 564.8, 4652.3, {d: 654.7, color: "selected"}, 2014.5, 3333.3, 1111.1, 2345.6],
-        ["Twitter", 555.5, "commercial", 444.4, 333.3, 222.2, 1111.1, 999.9, 555.5, 444.4, 222.2],
-        ["Glovo", 987.6, "commercial", 543.2, {d: 789.1, color: "selected"}, 234.5, 657.8, 123.4, 666.6, 222.2, 555.5],
-        ["YouTube", 1234.5, "internal", {d: 876.5, color: "selected"}, 543.2, 987.6, 345.2, 1111.1, 444.4, 333.3, 333.3],
-        ["GoDee", 6543.2, "commercial", 987.6, 765.4, {d: 987.6, color: "selected"}, 876.5, 654.3, 543.2, 432.1, 321.0],
-        ["Enji", 10000.0, "commercial", 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, {d: 7000.0, color: "selected"}, {d: 8000.0, color: "selected"}, {d: 9000.0, color: "selected"}],
+        ["Online-School", true, 1024.3, "commercial", 2048.2, 4096.0, 1058.1, 941.7, 4519.4, 2013.1, 513.1, 907.3],
+        ["City-Scope", true, 2345.1, "internal", 3465.3, {d: 12.5, color: "selected"}, 547.0, 5416.1, 1654.8, 987.4, 500.0, 789.5],
+        ["Spotify", false, null, "commercial"],
+        ["Twitter", true, 555.5, "commercial", 444.4, 333.3, 222.2, 1111.1, 999.9, 555.5, 444.4, 222.2],
+        ["Glovo", false, null, "commercial"],
+        ["YouTube", true, 1234.5, "internal", {d: 876.5, color: "selected"}, 543.2, 987.6, 345.2, 1111.1, 444.4, 333.3, 333.3],
+        ["GoDee", true, 6543.2, "commercial", 987.6, 765.4, {d: 987.6, color: "selected"}, 876.5, 654.3, 543.2, 432.1, 321.0],
+        ["Enji", true, 10000.0, "commercial", 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, {d: 7000.0, color: "selected"}, {d: 8000.0, color: "selected"}, {d: 9000.0, color: "selected"}],
     ]
 };
 
